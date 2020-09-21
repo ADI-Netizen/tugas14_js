4446644109a846cf1f9a40bee24168e6f29c995d
ADI-Netizen <71365604+ADI-Netizen@users.noreply.github.com>
2020-09-21 09:27:28 +0700

    Add files via upload

---
+++ tugas14.js
@@ -0,0 +1,9 @@
+var angka = 37
+var tanggal = 16
+
+pembagian = angka / tanggal
+
+console.log(pembagian)
+console.log("Math Round = ", Math.round(pembagian))
+console.log("Math Ceil = ", Math.ceil(pembagian))
+console.log("Math Floor = ", Math.floor(pembagian))
